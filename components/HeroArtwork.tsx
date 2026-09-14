29356f6f1413e873ad7f1281809309f3d639995f
BITMATE-del <gimg76622@gmail.com>
2026-09-14 13:45:25 +0900

    Point hero artwork to uploaded WebP asset

--- components/HeroArtwork.tsx
+++ components/HeroArtwork.tsx
@@ -1,3 +1,3 @@
 export default function HeroArtwork(){
-  return <div className="heroArtworkWrap"><img className="heroArtwork" src="/bitmate-hero.svg" alt="BITMATE trading platform visual"/></div>;
+  return <div className="heroArtworkWrap"><img className="heroArtwork" src="/bitmate-hero.webp" alt="BITMATE trading platform visual"/></div>;
 }
