520006dde0142ce0091db700da82e2d24b3d327e
BITMATE-del <gimg76622@gmail.com>
2026-09-14 14:02:18 +0900

    Add Mining user page

---
+++ app/mining/page.tsx
@@ -0,0 +1,3 @@
+import MiningGame from '@/components/MiningGame';
+export const metadata={title:'Mining | BITMATE',description:'Premium arcade-style digital mining experience'};
+export default function MiningPage(){return <MiningGame/>;}
