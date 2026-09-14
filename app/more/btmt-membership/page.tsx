9c9bee26910655622db7bf4353afa5277b447ea6
BITMATE-del <gimg76622@gmail.com>
2026-09-14 17:28:30 +0900

    Add BTMT membership page

---
+++ app/more/btmt-membership/page.tsx
@@ -0,0 +1,3 @@
+import BtmtMembershipClient from '@/components/BtmtMembershipClient';
+export const metadata={title:'BTMT Membership | BITMATE'};
+export default function Page(){return <BtmtMembershipClient/>}
