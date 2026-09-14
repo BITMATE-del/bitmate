90597c3fd73b433f99b8373b5e810acc4eea33e3
BITMATE-del <gimg76622@gmail.com>
2026-09-14 13:40:45 +0900

    Fix hero artwork to use embedded SVG asset

--- components/HeroArtwork.tsx
+++ components/HeroArtwork.tsx
@@ -1,10 +1,3 @@
-'use client';
-import {useEffect,useState} from 'react';
-
 export default function HeroArtwork(){
-  const [src,setSrc]=useState('');
-  useEffect(()=>{
-    fetch('/hero-data.txt').then(r=>r.text()).then(setSrc).catch(()=>setSrc(''));
-  },[]);
-  return <div className="heroArtworkWrap">{src?<img className="heroArtwork" src={src} alt="BITMATE trading platform visual"/>:<div className="heroArtworkLoading"/>}</div>;
+  return <div className="heroArtworkWrap"><img className="heroArtwork" src="/bitmate-hero.svg" alt="BITMATE trading platform visual"/></div>;
 }
