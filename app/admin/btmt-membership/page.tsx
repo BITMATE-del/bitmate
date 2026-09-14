888d130bfa484fb393ce934a9d8cfc010b7636cd
BITMATE-del <gimg76622@gmail.com>
2026-09-14 17:30:37 +0900

    Add BTMT membership admin page

---
+++ app/admin/btmt-membership/page.tsx
@@ -0,0 +1,3 @@
+import AdminBtmtMembershipClient from '@/components/AdminBtmtMembershipClient';
+export const metadata={title:'BTMT Membership Admin | BITMATE'};
+export default function Page(){return <AdminBtmtMembershipClient/>}
