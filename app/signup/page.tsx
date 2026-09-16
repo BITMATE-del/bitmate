c8fb6bf6ad26b898f816f1bff3aa8c9c3356e80c
BITMATE-del <gimg76622@gmail.com>
2026-09-16 15:18:03 +0900

    Add BITMATE signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,7 @@
+import AuthPortal from '@/components/AuthPortal';
+
+export const metadata={title:'회원 가입 | BITMATE'};
+
+export default function SignupPage(){
+  return <AuthPortal mode="signup"/>;
+}
