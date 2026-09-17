910d3401e5b2d1332639d43ac0268b70b34fbcc1
BITMATE-del <gimg76622@gmail.com>
2026-09-17 16:17:47 +0900

    Add crypto deposit route

---
+++ app/deposit/page.tsx
@@ -0,0 +1,6 @@
+import ExchangeHeader from '@/components/ExchangeHeader';
+import DepositClient from '@/components/DepositClient';
+
+export default function DepositPage(){
+  return <><ExchangeHeader/><DepositClient/></>;
+}
